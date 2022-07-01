b9f73b0e03cdfbb0f2ea80cccac5bea303db3510
agray998 <a.gray.4797@gmail.com>
2022-07-01 11:18:47 +0100

    java app and test script, updated workflow

---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+declare -a cases=(Alice Bob Claire Derek)
+
+for name in ${cases[@]}; do
+  if [ "$(java App ${name})" == "Hello, ${name}" ]; then
+    echo "Test Passed!"
+  else
+    echo "Test Failed!"
+    exit 1
+  fi
+done
