3514283b2ddbfd7167e940c0a2f013ba2f62da19
agray998 <a.gray.4797@gmail.com>
2022-07-01 11:32:46 +0100

    update test script

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 declare -a cases=(Alice Bob Claire Derek)
 
 for name in ${cases[@]}; do
-  if [ "$(java App ${name})" == "Hello, ${name}!" ]; then
+  if [ "$(java App ${name})" == "Hello, ${name}!!" ]; then
     echo "Test Passed!"
   else
     echo "Test Failed!"
